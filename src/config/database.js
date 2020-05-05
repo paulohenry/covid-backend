5e6739d9f751839db0fa223aa81c81f75a7f1b65
paulohenry <paulo.junior.engenheiro@gmail.com>
2020-05-05 04:46:57 -0300

    base backend com eslint e database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'covid1234',
+  database: 'database_covid_dev_2',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
