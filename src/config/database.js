245a98681bffe5a85fe25f530d79d632c52a5ea4
paulohenry <paulo.junior.engenheiro@gmail.com>
2020-05-17 20:39:17 -0300

    bugfix variavel ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,9 @@ module.exports = {
 
     dialect: 'postgres' ,
     host:'http://34.46.76.49',
-    username:postgres,
-    password:docker,
-    database,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
     define:{
       timestamps:true,
       underscored:true,
